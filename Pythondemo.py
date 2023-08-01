ece91c98e7f3ff7c837bd0ec5aa14b2e38103b50
BogaDoga <bog_dan1981@mail.ru>
2023-08-01 22:02:48 +0300

    okay

--- Pythondemo.py
+++ Pythondemo.py
@@ -13,7 +13,7 @@ if name in ["Ben", "Patrick", "Patricia"]:
 
 print("\nHello " + name + " what would you like to order?\n")
 
-menu =("Latte\nBlack coffee\nGreen coffee\n")
+menu =("Latte\nBlack coffee\nGreen coffee\nPizza\nTea\nSushi")
 
 print(menu)
 
@@ -33,6 +33,34 @@ elif response == "Black coffee":
 
 elif response == "Green coffee":
   print("Your total is 8 $")
+
+elif response == "Tea":
+  print("Your total is 4 $")
+
+if response == "Sushi":
+  def calculate_total_cost(num_pieces):
+    cost_per_set = 5  # $5 for every set of 4 pieces
+    sets_needed = (num_pieces + 3) // 4  # Round up to the nearest set of 4 pieces
+    total_cost = sets_needed * cost_per_set
+    return total_cost
+
+def main():
+    try:
+        num_pieces = int(input("How many pieces of sushi would you like? "))
+        if num_pieces < 0:
+            print("Can you please try a different amounut.")
+        else:
+            total_cost = calculate_total_cost(num_pieces)
+            print(f"Total cost: ${total_cost}")
+    except ValueError:
+        print("Can you please try a different amounut.")
+
+if __name__ == "__main__":
+    main()
+  
+if response == "Pizza":
+   pizzatype = ("")
+
 
 #END
 else:
