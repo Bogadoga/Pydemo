5b60132aa2568399d8934d02b09850a91948fc12
BogaDoga <bog_dan1981@mail.ru>
2023-08-01 15:50:26 +0300

    okay

---
+++ Lists.py
@@ -0,0 +1,11 @@
+suplies = ["tent", "sleeping bags", "coffee", "marshmellows", "water"]
+
+camp_site = ["Crystal Lake", 404, 20, False]
+
+suplies.extend(["toilet paper", "phone"])
+
+suplies.pop(0)
+
+suplies.pop(0)
+
+print(suplies)
