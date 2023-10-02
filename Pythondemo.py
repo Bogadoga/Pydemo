cf8d634703a8470c4d76e5d3cb877573e6c8dd5b
BogaDoga <bog_dan1981@mail.ru>
2023-10-02 21:53:50 +0300

    new code

--- Pythondemo.py
+++ Pythondemo.py
@@ -1,68 +1,68 @@
-name =input("Hello welcome to our coffee shop!\nCan i get your name?\n\n")
+# Welcome message and input for the customer's name
+name = input("Hello, welcome to our coffee shop!\nCan I get your name?\n\n")
 
-
-#if the name is ben patrick or patricia and they are evil they will get kicked out.
+# Check if the name is in a predefined list
 if name in ["Ben", "Patrick", "Patricia"]:
-  evil =input("Are you evil?\n")
-  if evil == "Yes":
-    print ("GET OUT OF HERE YOUR NOT WELCOME")
-    exit()
-  else:
+    evil = input("Are you evil?\n")
     
-    print("Oh so your one of those good " +name)
-
-print("\nHello " + name + " what would you like to order?\n")
-
-menu =("Latte\nBlack coffee\nGreen coffee\nPizza\nTea\nSushi")
-
-print(menu)
-
-response =input()
-
-if response == "Latte":
-  cream = input("Would you like whipped cream?\n")
-  
-  if cream in ["Yes", "yes", "y"]:
-    print("Your total is 15 $")
-  else:
-    print("Your total is 9 $")
-    
-
-elif response == "Black coffee":
-  print("Your total is 5 $")
-
-elif response == "Green coffee":
-  print("Your total is 8 $")
-
-elif response == "Tea":
-  print("Your total is 4 $")
-
-if response == "Sushi":
-  def calculate_total_cost(num_pieces):
-    cost_per_set = 5  # $5 for every set of 4 pieces
-    sets_needed = (num_pieces + 3) // 4  # Round up to the nearest set of 4 pieces
-    total_cost = sets_needed * cost_per_set
-    return total_cost
-
-def main():
-    try:
-        num_pieces = int(input("How many pieces of sushi would you like? "))
-        if num_pieces < 0:
-            print("Can you please try a different amounut.")
-        else:
-            total_cost = calculate_total_cost(num_pieces)
-            print(f"Total cost: ${total_cost}")
-    except ValueError:
-        print("Can you please try a different amounut.")
-
-if __name__ == "__main__":
-    main()
-  
-if response == "Pizza":
-   pizzatype = ("")
-
-
-#END
+    # Check if the customer is evil
+    if evil.lower() == "yes":
+        print("GET OUT OF HERE YOU'RE NOT WELCOME")
+        exit()
+    else:
+        print("Oh, so you're one of those good " + name)
+elif name.lower() == "gloria":
+    print("\nOh, I know someone named Gloria. What would you like to order?\n")
 else:
-  print("Sorry we dont have that here.")
-  exit()
+    print("\nHello " + name + ", what would you like to order?\n")
+
+while True:
+    # Display the menu
+    menu = "Latte\nBlack coffee\nGreen coffee\nIce Cream\n"
+    print(menu)
+
+    # Get the customer's order
+    response = input().lower()
+
+    # Process the order
+    if response == "latte":
+        cream = input("Would you like whipped cream?\n")
+        
+        if cream.lower() in ["yes", "y"]:
+            print("Your total is $15")
+        else:
+            print("Your total is $9")
+    elif response == "black coffee":
+        print("Your total is $5")
+    elif response == "green coffee":
+        print("Your total is $8")
+    elif response == "ice cream":
+        flavour = input(
+            "\nWhat flavor would you like?\n\n"
+            "Strawberry flavor $14\n"
+            "Banana flavor $13\n"
+            "Vanilla flavor $12\n"
+            "Simple $10\n"
+            "Mango flavor $17\n\n"
+        ).lower()
+        
+        if flavour == "strawberry":
+            print("That's gonna be $14")
+        elif flavour == "banana":
+            print("That's gonna be $13")
+        elif flavour == "vanilla":
+            print("That's gonna be $12")
+        elif flavour == "simple":
+            print("That's gonna be $10")
+        elif flavour == "mango":
+            print("That's gonna be $17")
+        else:
+            print("Sorry, we don't have that flavor on the menu.")
+
+    # Ask if the customer wants to order again
+    order_again = input("Would you like to order again? (yes/no)\n").lower()
+    if order_again != "yes":
+        break
+
+# Farewell message
+print("Thank you for visiting our coffee shop, " + name + "! Goodbye!")
